2aadfef7680243363733573a8b1a631824bfb3c7
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-18 23:33:48 +0530

    Product CRUD implemented

---
+++ product/src/main/java/com/nipun/product/dto/Product.java
@@ -0,0 +1,14 @@
+package com.nipun.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String productCode;
+    private String name;
+    private Double price;
+}
