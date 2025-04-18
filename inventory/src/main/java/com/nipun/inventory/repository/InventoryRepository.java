ed4c5e715ae2a6bb194d983d3bbe567e1cd17df7
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-19 01:56:56 +0530

    Create Multimodule Project and Remove Individual Projects

--- inventory/src/main/java/com/nipun/inventory/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nipun.inventory.repository;
-
-import com.nipun.inventory.entity.InventoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
-}
--- product/src/main/java/com/nipun/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nipun.product.repository;
-
-import com.nipun.product.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
