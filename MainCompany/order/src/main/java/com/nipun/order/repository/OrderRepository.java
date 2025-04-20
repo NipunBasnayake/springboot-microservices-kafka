e97a14a6e993c72aad451a437ccd27234aedb371
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-04-20 12:16:33 +0530

    Update DTOs

--- MainCompany/order/src/main/java/com/nipun/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nipun.order.repository;
-
-import com.nipun.order.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-}
